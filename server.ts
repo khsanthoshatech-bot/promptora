54bfe0462de8113b4ea10b201ff622fb49b6d33c
KH Santhosha <khsanthosha.tech@gmail.com>
2026-06-16 00:39:04 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ export const prisma = new PrismaClient();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(cors({
     origin: '*',
